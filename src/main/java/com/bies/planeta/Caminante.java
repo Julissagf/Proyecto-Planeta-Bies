acc689bd520e8c553aab087da09e2893ff4337d7
PabloSA12 <pablosegas@gmail.com>
2024-09-01 09:39:20 -0600

    Caminante.java

--- src/main/java/com/bies/planeta/Caminante.java
+++ src/main/java/com/bies/planeta/Caminante.java
@@ -3,10 +3,34 @@ package com.bies.planeta;
 public class Caminante extends EnteVivoDecorador {
     private int patas;
     private static final Random random = new Random();
+
+    public Caminante (EnteVivo ente) {
+        super(ente);
+    }
   
-  @Override
-  public String movilizarse() {
-     if (patas > 2) {
+    @Override
+    public String movilizarse() {
+        return "Ya no puedo movilizarme porque soy carroña";
+    }
+
+    @Override
+    public void setAlimentarse(Alimentarse a) {
+        getEnte().setAlimentarse(a);
+    }
+    
+    @Override
+    public String comer() {
+        getEnte().comer();
+    }
+
+    @Override
+    public String toString() {
+        getEnte().toString();
+    }
+  
+    @Override
+    public String movilizarse() {
+        if (patas > 2) {
             if (random.nextDouble() < 1.0 / patas) {
                 patas--;
                 return "El ser vivo ha perdido una pata. Patas restantes: " + patas;
@@ -15,5 +39,5 @@ public class Caminante extends EnteVivoDecorador {
             
             return "El ser vivo no puede caminar porque no tiene patas.";
         }
-  }
+    }
 }
