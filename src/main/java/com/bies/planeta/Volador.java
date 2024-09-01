2cffdf82c0978e6b620ce25959503382ed00c1a4
PabloSA12 <pablosegas@gmail.com>
2024-09-01 09:40:19 -0600

    Volador.java

--- src/main/java/com/bies/planeta/Volador.java
+++ src/main/java/com/bies/planeta/Volador.java
@@ -3,10 +3,34 @@ package com.bies.planeta;
 public class Volador extends EnteVivoDecorador {
     private int alas;
     private static final Random random = new Random();
+
+    public Volador (EnteVivo ente) {
+        super(ente);
+    }
   
-  @Override
-  public String movilizarse() {
-     if (patas > 2) {
+    @Override
+    public String movilizarse() {
+        return "Ya no puedo movilizarme porque soy carroña";
+    }
+
+    @Override
+    public void setAlimentarse(Alimentarse a) {
+        getEnte().setAlimentarse(a);
+    }
+    
+    @Override
+    public String comer() {
+        getEnte().comer();
+    }
+
+    @Override
+    public String toString() {
+        getEnte().toString();
+    }
+    
+    @Override
+    public String movilizarse() {
+        if (patas > 2) {
             if (random.nextDouble() < 1.0 / patas) {
                 patas--;
                 return "El ser vivo ha perdido un ala. Alas restantes: " + alas;
@@ -15,5 +39,5 @@ public class Volador extends EnteVivoDecorador {
             
             return "El ser vivo no puede volar porque no tiene patas.";
         }
-  }
+    }
 }
