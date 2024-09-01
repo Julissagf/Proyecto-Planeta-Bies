4b51836a74fe97d0a278a36eb38d640fab93c510
PabloSA12 <pablosegas@gmail.com>
2024-09-01 09:58:55 -0600

    Caminante.java

--- src/main/java/com/bies/planeta/Caminante.java
+++ src/main/java/com/bies/planeta/Caminante.java
@@ -1,5 +1,7 @@
 package com.bies.planeta;
 
+import java.util.Random;
+
 public class Caminante extends EnteVivoDecorador {
     private int patas;
     private static final Random random = new Random();
