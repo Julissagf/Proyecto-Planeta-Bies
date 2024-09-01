0fd0114bb1511eb04a4c6db5d9d50a3f8b4a9ea7
PabloSA12 <pablosegas@gmail.com>
2024-09-01 09:22:53 -0600

    Carroña.java

--- src/main/java/com/bies/planeta/Carroña.java
+++ src/main/java/com/bies/planeta/Carroña.java
@@ -3,6 +3,6 @@ package com.bies.planeta;
 public class Carroña extends EnteVivoDecorador {
   @Override
   public String movilizarse() {
-     return "Soy carroña";
+     return "Ya no puedo movilizarme porque soy carroña";
   }
 }
