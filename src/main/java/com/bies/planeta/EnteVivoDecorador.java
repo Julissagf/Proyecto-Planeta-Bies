e98a68b9d705ffaa41e311a194f74fcb09422ab7
PabloSA12 <pablosegas@gmail.com>
2024-09-01 09:12:01 -0600

    EnteVivoDecorador.java

--- src/main/java/com/bies/planeta/EnteVivoDecorador.java
+++ src/main/java/com/bies/planeta/EnteVivoDecorador.java
@@ -1,5 +1,15 @@
 package com.bies.planeta;
 
-public abstract EnteVivoDecorador {
+public abstract EnteVivoDecorador extends EnteVivo {
+  private EnteVivo ente;
+
+  public EnteVivoDecorador (EnteVivo e) {
+    ente = e;
+  }
+
+  protected EnteVivo getEnte () {
+    return ente;
+  }
+  
   public String movilizarse();
 }
