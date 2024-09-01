5184dc54701b248db145773222297e4ad4df5ec0
PabloSA12 <pablosegas@gmail.com>
2024-09-01 09:51:33 -0600

    Caminante.java

--- src/main/java/com/bies/planeta/Caminante.java
+++ src/main/java/com/bies/planeta/Caminante.java
@@ -4,13 +4,23 @@ public class Caminante extends EnteVivoDecorador {
     private int patas;
     private static final Random random = new Random();
 
-    public Caminante (EnteVivo ente) {
+    public Caminante(EnteVivo ente, int patasIniciales) {
         super(ente);
+        this.patas = patasIniciales;
     }
-  
+
     @Override
     public String movilizarse() {
-        return "Ya no puedo movilizarme porque soy carroña";
+        if (patas > 2) {
+            if (random.nextDouble() < 1.0 / patas) {
+                patas--;
+                return "El ser vivo ha perdido una pata. Patas restantes: " + patas;
+            } else {
+                return "El ser vivo se mueve con " + patas + " patas.";
+            }
+        } else {
+            return "El ser vivo no puede caminar porque no tiene patas. Ahora es carroña.";
+        }
     }
 
     @Override
@@ -20,24 +30,11 @@ public class Caminante extends EnteVivoDecorador {
     
     @Override
     public String comer() {
-        getEnte().comer();
+        return getEnte().comer();
     }
 
     @Override
     public String toString() {
-        getEnte().toString();
-    }
-  
-    @Override
-    public String movilizarse() {
-        if (patas > 2) {
-            if (random.nextDouble() < 1.0 / patas) {
-                patas--;
-                return "El ser vivo ha perdido una pata. Patas restantes: " + patas;
-            }
-        } else {
-            
-            return "El ser vivo no puede caminar porque no tiene patas.";
-        }
+        return getEnte().toString() + " (Caminante con " + patas + " patas)";
     }
 }
