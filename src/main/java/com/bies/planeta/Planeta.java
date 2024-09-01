a500e13d8375e32f5b9cf99ad8a97dae50e8d26c
PabloSA12 <pablosegas@gmail.com>
2024-09-01 08:52:02 -0600

    Planeta.java

--- src/main/java/com/bies/planeta/Planeta.java
+++ src/main/java/com/bies/planeta/Planeta.java
@@ -1,2 +1,17 @@
 package com.bies.planeta;
 
+public class Planeta {
+  private String nombre;
+
+  public Planeta(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
