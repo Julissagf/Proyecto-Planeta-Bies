041bc4a0293f2dee19bb34958c9b20335fac1338
PabloSA12 <pablosegas@gmail.com>
2024-09-02 13:22:21 -0600

    Mariposa.java

--- src/main/java/com/bies/planeta/Mariposa.java
+++ src/main/java/com/bies/planeta/Mariposa.java
@@ -1,7 +1,26 @@
 package com.bies.planeta;
 
+/**
+ * Representa una araña en el sistema. La mariposa es un tipo de insecto que se alimenta
+ * inicialmente de miel, pero su dieta puede ser modificada utilizando el patrón Strategy.
+ * 
+ * <p>Autores: 
+ * Fernando Acuña Zeledón - 1-1890-0988, 
+ * Julissa Gómez Fuentes - 1-1890-0658, 
+ * Julián David Ramírez Salas - 1-1874-0667, 
+ * Juan Pablo Segura Astúa - 1-1875-0806
+ * 
+ * <p>Fecha: 2024-09-02
+ * 
+ * <p>Propósito: Implementar una clase que representa una mariposa, la cual puede alimentarse
+ * de diferentes tipos de alimentos según el contexto.
+ */
+
 public class Mariposa extends Insecto {
 
+    /**
+     * Constructor que inicializa la dieta de la mariposa a alimentarse de miel.
+     */
     public Mariposa(){ //Strategy
         alimentarse = new AlimentarseDeMiel();
     }
