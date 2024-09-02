c394a295fcbfbe370a84620509ef2ed2ea4280bb
PabloSA12 <pablosegas@gmail.com>
2024-09-02 13:29:08 -0600

    Volador.java

--- src/main/java/com/bies/planeta/Volador.java
+++ src/main/java/com/bies/planeta/Volador.java
@@ -2,16 +2,43 @@ package com.bies.planeta;
 
 import java.util.Random;
 
+/**
+ * Decorador que permite a un ente vivo adquirir la habilidad de volar.
+ *
+ * <p>Propósito: Implementar la habilidad de volar para un ente vivo y gestionar
+ * la degradación motora cuando pierde sus patas.
+ *
+ * <p>Autores:
+ * - Fernando Acuña Zeledón (1-1890-0988)
+ * - Julissa Gómez Fuentes (1-1890-0658)
+ * - Julián David Ramírez Salas (1-1874-0667)
+ * - Juan Pablo Segura Astúa (1-1875-0806)
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
 public class Volador extends EnteVivoDecorador {
     private int alas;
     private static final Random random = new Random();
 
+    /**
+     * Constructor que inicializa un ente caminante con un número específico de alas.
+     *
+     * @param ente El ente vivo que se decorará con la habilidad de volar.
+     * @param patasIniciales El número inicial de alas del ente.
+     */
     public Volador(EnteVivo ente, int alasIniciales) {
         super(ente);
         this.alas = alasIniciales;
     }
 
-
+    /**
+     * Método que permite al ente movilizarse volando. Si pierde todas sus alas,
+     * se convierte en carroña.
+     *
+     * @return Una cadena que describe el estado actual del ente al volar.
+     */
     public String movilizarse() {
         if (alas > 2) {
             if (random.nextDouble() < 1.0 / alas) {
@@ -26,16 +53,31 @@ public class Volador extends EnteVivoDecorador {
         }
     }
 
+    /**
+     * Establece la estrategia de alimentación para el ente volador.
+     *
+     * @param a Estrategia de alimentación a ser establecida.
+     */
     @Override
     public void setAlimentarse(Alimentarse a) {
         getEnte().setAlimentarse(a);
     }
-    
+
+    /**
+     * Describe cómo el ente volador se alimenta.
+     *
+     * @return Una cadena que describe la acción de comer del ente.
+     */
     @Override
     public String comer() {
         return getEnte().comer();
     }
 
+    /**
+     * Retorna una descripción del ente volador, incluyendo el número de alas.
+     *
+     * @return Una cadena que describe al ente volador.
+     */
     @Override
     public String toString() {
         return getEnte().toString() + " (Volador con " + alas + " alas)";
