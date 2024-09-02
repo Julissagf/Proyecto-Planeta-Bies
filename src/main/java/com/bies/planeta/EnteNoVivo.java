c1d015321dc0867152945cbe2074e88723436c95
PabloSA12 <pablosegas@gmail.com>
2024-09-02 14:12:52 -0600

    EnteNoVivo.java

--- src/main/java/com/bies/planeta/EnteNoVivo.java
+++ src/main/java/com/bies/planeta/EnteNoVivo.java
@@ -1,17 +1,45 @@
 package com.bies.planeta;
 
+/**
+ * Representa un ente no vivo en el sistema.
+ *
+ * <p>Propósito: Modelar entidades que han perdido su vitalidad y ya no pueden
+ * realizar funciones básicas como alimentarse o movilizarse.
+ *
+ * <p>Autores:
+ * - Fernando Acuña Zeledón (1-1890-0988)
+ * - Julissa Gómez Fuentes (1-1890-0658)
+ * - Julián David Ramírez Salas (1-1874-0667)
+ * - Juan Pablo Segura Astúa (1-1875-0806)
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
 public abstract class EnteNoVivo extends Ente {
 
-   
+   /**
+     * Retorna una descripción del ente no vivo.
+     *
+     * @return Una cadena que describe al ente no vivo.
+     */
     @Override
     public abstract String toString();
 
+   /**
+     * Describe que el ente no vivo no puede comer.
+     *
+     * @return Una cadena que indica que el ente no puede comer.
+     */
+    @Override
     @Override
     public String comer(){ return "No puedo comer, porque soy un ente no vivo"; }
 
+   /**
+     * Método que no tiene implementación en entes no vivos.
+     *
+     * @param s No tiene efecto en un ente no vivo.
+     */
     @Override
     public void setAlimentarse(Alimentarse s){ }
-
- 
-    
 }
