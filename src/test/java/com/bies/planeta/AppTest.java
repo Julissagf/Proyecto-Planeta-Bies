d4df110b81b51a9e4207d94ecf685cb76f367c6b
fernando2044 <fernando.acuna.zeledon@est.una.ac.cr>
2024-09-02 17:32:23 -0600

    Agregado todos los casos de pruebas unitarias.

--- src/test/java/com/bies/planeta/AppTest.java
+++ src/test/java/com/bies/planeta/AppTest.java
@@ -1,6 +1,8 @@
 package com.bies.planeta;
 
+import com.bies.planeta.entes.Mariposa;
 import com.bies.planeta.entes.Mosca;
+import com.bies.planeta.entes.Arana;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -36,21 +38,51 @@ public class AppTest
         assertTrue( true );
     }
 
+    /**
+     * Prueba unitaria de una mosca comiendo.
+     */
     public void testMoscaComer() {
         Mosca mosca =  new Mosca();
-
-        // System.err.println(comerResult);
-        // System.err.println(mosca.toString());
-
         assertNotNull(mosca.comer());
     }
 
+    /**
+     * Prueba unitaria de una mosca movilizandose.
+     */
     public void testMoscaMovilizar() {
         Mosca mosca =  new Mosca();
-
-        // System.err.println(mosca.movilizarse());
-        // System.err.println(mosca.toString());
-
         assertNotNull(mosca.movilizarse());
+    }
+    
+    /**
+     * Prueba unitaria de una mariposa comiendo.
+     */
+    public void testMariposaComer() {
+        Mariposa mariposa =  new Mariposa();
+        assertNotNull(mariposa.comer());
+    }
+
+    /**
+     * Prueba unitaria de una mariposa movilizandose.
+     */
+    public void testMariposaMovilizar() {
+        Mariposa mariposa =  new Mariposa();
+        assertNotNull(mariposa.movilizarse());
+    }
+
+    /**
+     * Prueba unitaria de una araña comiendo.
+     */
+    public void testAranaComer() {
+        Arana arana =  new Arana();
+        assertNotNull(arana.comer());
+    }
+
+    /**
+     * Prueba unitaria de una araña movilizandose.
+     */
+    public void testAranaMovilizar() {
+        Arana arana =  new Arana();
+        assertNotNull(arana.movilizarse());
     }
 }
