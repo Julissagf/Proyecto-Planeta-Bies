434e5f4997641c3d1104551f978b95a9901e3f3d
fernando2044 <fernando.acuna.zeledon@est.una.ac.cr>
2024-09-01 21:44:52 -0600

    Agregado lista de decorators en la clase abstracta "EnteVivo".

--- src/main/java/com/bies/planeta/EnteVivo.java
+++ src/main/java/com/bies/planeta/EnteVivo.java
@@ -1,9 +1,13 @@
 package com.bies.planeta;
+import java.util.ArrayList;
 
 public abstract class EnteVivo {
     Alimentarse alimentarse;
+    ArrayList<EnteVivoDecorador> decorators = new ArrayList<>();
 
     public abstract void setAlimentarse(Alimentarse a);
     public abstract String comer();
     public abstract String toString();
+
+    public void addDecorator(EnteVivoDecorador e) { decorators.add(e); }
 }
