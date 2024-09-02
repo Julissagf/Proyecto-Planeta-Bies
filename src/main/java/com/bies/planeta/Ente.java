b47d259b5db2a77cdf8aa7b454de186c100ec2e0
PabloSA12 <pablosegas@gmail.com>
2024-09-02 13:13:43 -0600

    Ente.java

--- src/main/java/com/bies/planeta/Ente.java
+++ src/main/java/com/bies/planeta/Ente.java
@@ -1,9 +1,42 @@
 package com.bies.planeta;
 
+/**
+ * Clase abstracta que representa un ente, el cual puede ser vivo o no vivo.
+ *
+ * <p>Propósito: Establecer una base común para todos los entes, con métodos que deben
+ * ser implementados por las subclases específicas.
+ *
+ * <p>Autores:
+ * - Fernando Acuña Zeledón (1-1890-0988)
+ * - Julissa Gómez Fuentes (1-1890-0658)
+ * - Julián David Ramírez Salas (1-1874-0667)
+ * - Juan Pablo Segura Astúa (1-1875-0806)
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
 public abstract class Ente {
 
+    /**
+     * Método abstracto que retorna una descripción del ente.
+     *
+     * @return Una cadena que describe el ente.
+     */
     public abstract String toString();
+
+    /**
+     * Método abstracto que define cómo el ente se alimenta.
+     *
+     * @return Una cadena que describe la acción de comer.
+     */
     public abstract String comer();
+
+    /**
+     * Método abstracto para establecer la estrategia de alimentación del ente.
+     *
+     * @param s Estrategia de alimentación a establecer.
+     */
     public abstract void setAlimentarse(Alimentarse s);
 
 }
