688f68b915c73da92289c9062670bfe018610252
fernando2044 <fernando.acuna.zeledon@est.una.ac.cr>
2024-09-02 17:20:32 -0600

    Agregados test cases "testMoscaComer()" y "testMoscaMovilizar()".

--- src/test/java/com/bies/planeta/AppTest.java
+++ src/test/java/com/bies/planeta/AppTest.java
@@ -1,5 +1,7 @@
 package com.bies.planeta;
 
+import com.bies.planeta.entes.Mosca;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -32,5 +34,23 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    public void testMoscaComer() {
+        Mosca mosca =  new Mosca();
+
+        // System.err.println(comerResult);
+        // System.err.println(mosca.toString());
+
+        assertNotNull(mosca.comer());
+    }
+
+    public void testMoscaMovilizar() {
+        Mosca mosca =  new Mosca();
+
+        // System.err.println(mosca.movilizarse());
+        // System.err.println(mosca.toString());
+
+        assertNotNull(mosca.movilizarse());
     }
 }
