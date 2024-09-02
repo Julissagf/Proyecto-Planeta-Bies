79ffd46c3495802a7df36efb51e05f71dcad228b
PabloSA12 <pablosegas@gmail.com>
2024-09-02 14:16:17 -0600

    EnteVivoDecorador.java

--- src/main/java/com/bies/planeta/EnteVivoDecorador.java
+++ src/main/java/com/bies/planeta/EnteVivoDecorador.java
@@ -1,20 +1,54 @@
 package com.bies.planeta;
 
+/**
+ * Decorador abstracto para los entes vivos, que permite añadirles nuevas capacidades.
+ *
+ * <p>Propósito: Proveer una estructura base para decoradores que añaden funcionalidades
+ * adicionales a los entes vivos, como habilidades de movimiento o alimentación.
+ *
+ * <p>Autores:
+ * - Fernando Acuña Zeledón (1-1890-0988)
+ * - Julissa Gómez Fuentes (1-1890-0658)
+ * - Julián David Ramírez Salas (1-1874-0667)
+ * - Juan Pablo Segura Astúa (1-1875-0806)
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
 public abstract class EnteVivoDecorador extends EnteVivo {
     private Ente ente;
 
+    /**
+     * Constructor que inicializa el decorador con el ente a decorar.
+     *
+     * @param e El ente vivo que será decorado.
+     */
     public EnteVivoDecorador(EnteVivo e) {
         this.ente = e;
     }
 
+    /**
+     * Obtiene el ente decorado.
+     *
+     * @return El ente decorado.
+     */
     protected Ente getEnte() {
         return ente;
     }
 
+    /**
+     * Establece un nuevo ente a decorar.
+     *
+     * @param nuevoEnte El nuevo ente que será decorado.
+     */
     protected void setEnte(Ente nuevoEnte) {
         this.ente = nuevoEnte;
     }
 
+    /**
+     * Degrada el ente vivo a un ente no vivo (carroña).
+     */
     void degradacionMotora(){
         setEnte(new Carroña());
     }
