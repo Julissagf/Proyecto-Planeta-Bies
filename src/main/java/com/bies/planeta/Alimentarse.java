0e56eca07f08a8ee0cdb37c0e8a14fac89084191
PabloSA12 <pablosegas@gmail.com>
2024-09-02 13:12:59 -0600

    Alimentarse.java

--- src/main/java/com/bies/planeta/Alimentarse.java
+++ src/main/java/com/bies/planeta/Alimentarse.java
@@ -1,5 +1,28 @@
 package com.bies.planeta;
 
+/**
+ * Interfaz que define el comportamiento de alimentación para los entes.
+ *
+ * <p>Propósito: Proveer una interfaz común para diferentes estrategias de alimentación
+ * que pueden ser implementadas por diversos tipos de entes.
+ *
+ * <p>Autores:
+ * - Fernando Acuña Zeledón (1-1890-0988)
+ * - Julissa Gómez Fuentes (1-1890-0658)
+ * - Julián David Ramírez Salas (1-1874-0667)
+ * - Juan Pablo Segura Astúa (1-1875-0806)
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
 public interface Alimentarse {
+
+    /**
+     * Método que define cómo un ente se alimenta.
+     *
+     * @return Una cadena que describe la acción de alimentarse.
+     */
     String alimentarse();
+    
 }
