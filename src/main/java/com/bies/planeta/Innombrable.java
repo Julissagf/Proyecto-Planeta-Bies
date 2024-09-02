1b3690fefee3ae434a85e8ffd69148ed4bd59c3b
PabloSA12 <pablosegas@gmail.com>
2024-09-02 14:15:11 -0600

    Innombrable.java

--- src/main/java/com/bies/planeta/Innombrable.java
+++ src/main/java/com/bies/planeta/Innombrable.java
@@ -1,16 +1,47 @@
 package com.bies.planeta;
 
+/**
+ * Representa un ente vivo que es innombrable.
+ *
+ * <p>Propósito: Modelar un ente con características específicas y sin nombre definido.
+ *
+ * <p>Autores:
+ * - Fernando Acuña Zeledón (1-1890-0988)
+ * - Julissa Gómez Fuentes (1-1890-0658)
+ * - Julián David Ramírez Salas (1-1874-0667)
+ * - Juan Pablo Segura Astúa (1-1875-0806)
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
 public class Innombrable extends EnteVivo{
+    
+    /**
+     * Establece la estrategia de alimentación para el innombrable.
+     *
+     * @param a Estrategia de alimentación a ser establecida.
+     */
     @Override
     public void setAlimentarse(Alimentarse a){
         alimentarse = a;
     }
 
+    /**
+     * Describe cómo el innombrable se alimenta.
+     *
+     * @return Una cadena que describe la acción de comer del innombrable.
+     */
     @Override
     public String comer(){
         return alimentarse.alimentarse();
     }
 
+    /**
+     * Retorna una descripción del innombrable.
+     *
+     * @return Una cadena que describe al innombrable.
+     */
     @Override
     public String toString(){
         return "Soy un innombrable.";
