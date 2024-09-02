3ec923b4021aaeedaa3be0b403fa6eecb5e7d693
PabloSA12 <pablosegas@gmail.com>
2024-09-02 13:16:03 -0600

    AlimentarseDeMiel.java

--- src/main/java/com/bies/planeta/AlimentarseDeMiel.java
+++ src/main/java/com/bies/planeta/AlimentarseDeMiel.java
@@ -1,7 +1,27 @@
 package com.bies.planeta;
 
+/**
+ * Esta clase define la estrategia de alimentación de miel.
+ *
+ * <p>Propósito: Implementar la estrategia específica de alimentación basada en miel.
+ *
+ * <p>Autores:
+ * - Fernando Acuña Zeledón (1-1890-0988)
+ * - Julissa Gómez Fuentes (1-1890-0658)
+ * - Julián David Ramírez Salas (1-1874-0667)
+ * - Juan Pablo Segura Astúa (1-1875-0806)
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
 public class AlimentarseDeMiel implements Alimentarse {
-    
+
+    /**
+     * Método que define cómo se alimenta un ente de miel.
+     *
+     * @return Una cadena que describe la acción de alimentarse de miel.
+     */
     public String alimentarse(){
         return "Me alimento de Miel";
     }
