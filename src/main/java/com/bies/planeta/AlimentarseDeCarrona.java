2ffb298377f0802e56b824df9f8863e0227f1e54
PabloSA12 <pablosegas@gmail.com>
2024-09-02 13:14:18 -0600

    AlimentarseDeCarrona.java

--- src/main/java/com/bies/planeta/AlimentarseDeCarrona.java
+++ src/main/java/com/bies/planeta/AlimentarseDeCarrona.java
@@ -1,7 +1,27 @@
 package com.bies.planeta;
 
+/**
+ * Esta clase define la estrategia de alimentación de Carroña.
+ *
+ * <p>Propósito: Implementar la estrategia específica de alimentación basada en Carroña.
+ *
+ * <p>Autores:
+ * - Fernando Acuña Zeledón (1-1890-0988)
+ * - Julissa Gómez Fuentes (1-1890-0658)
+ * - Julián David Ramírez Salas (1-1874-0667)
+ * - Juan Pablo Segura Astúa (1-1875-0806)
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
 public class AlimentarseDeCarrona implements Alimentarse {
-     
+
+     /**
+     * Método que define cómo se alimenta un ente de carroña.
+     *
+     * @return Una cadena que describe la acción de alimentarse de carroña.
+     */
     public String alimentarse(){
         return "Me alimento de Carroña";
     }
