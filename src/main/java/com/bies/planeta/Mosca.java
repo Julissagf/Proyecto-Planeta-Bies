e3ab4830e6ed424e962400f1914e99c6ee680362
PabloSA12 <pablosegas@gmail.com>
2024-09-02 13:21:14 -0600

    Mosca.java

--- src/main/java/com/bies/planeta/Mosca.java
+++ src/main/java/com/bies/planeta/Mosca.java
@@ -1,8 +1,26 @@
 package com.bies.planeta;
 
+/**
+ * Representa una araña en el sistema. La mosca es un tipo de insecto que se alimenta
+ * inicialmente de carroña, pero su dieta puede ser modificada utilizando el patrón Strategy.
+ * 
+ * <p>Autores: 
+ * Fernando Acuña Zeledón - 1-1890-0988, 
+ * Julissa Gómez Fuentes - 1-1890-0658, 
+ * Julián David Ramírez Salas - 1-1874-0667, 
+ * Juan Pablo Segura Astúa - 1-1875-0806
+ * 
+ * <p>Fecha: 2024-09-01
+ * 
+ * <p>Propósito: Implementar una clase que representa una mosca, la cual puede alimentarse
+ * de diferentes tipos de alimentos según el contexto.
+ */
+
 public class Mosca extends Insecto {
 
-
+    /**
+     * Constructor que inicializa la dieta de la mosca a alimentarse de carroña.
+     */
     public Mosca(){ //Strategy
         alimentarse = new AlimentarseDeCarrona();
     }
